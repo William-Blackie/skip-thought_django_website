42f34aa1b026f4a526794d0214211017d548b22d
William Blackie <william.blackie96@gmail.com>
2019-03-01 21:52:47 +0000

    Adding some formatting and more outputs, issues with cudamemloc for second article

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from app.article import Article
 from skipthoughts import WebScraperSummation
+import thread
 
 from django.shortcuts import render
 
@@ -32,19 +33,20 @@ class ResearchPageView(TemplateView):
 
 
 class SummariserPageView(TemplateView):
-    def get(self, request, **kwargs):
+    def get(self, request):
         form = PostForm()
         return render(request, 'summariser.html',  {'form': form})
 
-    def post(self, request, *args, **kwargs):
-        scraper = WebScraperSummation.WebScraperSummation()
+    def post(self, request):
         if request.method == "POST":
             form = PostForm(request.POST)
             if form.is_valid():
                 print form.data.get('url')
                 print form.data.get('compression_rate')
                 print form.data.get('remove_lists')
+                scraper = WebScraperSummation.WebScraperSummation()
                 summary_text = scraper.scrape(form.data.get('url'), False, float(form.data.get('compression_rate')))
+                del scraper
                 article = Article(summary_text, form.data.get('url'), form.data.get('compression_rate'))  # Put into article object for easier indexing
                 return render(request, 'summary_output.html', {'article': article})  # Add article here
 
